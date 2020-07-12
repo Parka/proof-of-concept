4f37743c2ad1804f6bf16bff42ab814a9c7ff482
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-12 14:56:05 -0300

    Handle request and parse formData

--- thumbnail-generator-api/handler.js
+++ thumbnail-generator-api/handler.js
@@ -1,7 +1,16 @@
 'use strict';
 const Sharp = require('sharp');
+const Parser = require('lambda-multipart-parser');
 
-module.exports.resize = async event => {
+module.exports.resize = async (event) => {
+  const formData = await Parser.parse(event);
+  const {files:[original]} = formData;
+  if(!["image/png", "image/jpeg"].includes(original.contentType)) return {statusCode:400}
+  const sharp = Sharp(original.content);
+  console.log(sharp)
+  const metadata = await sharp.metadata();
+  console.log(metadata)
+
   return {
     statusCode: 200,
     body: JSON.stringify(
